f163e4c4a2ee29f5aeaba0ed27a330efec47fd1f
Johnson <oyesinajohnson@gmail.com>
2022-09-08 04:04:35 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of C program
+ * Return: 1 is success
+ */
+
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
