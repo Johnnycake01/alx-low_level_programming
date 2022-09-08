73aee3da1e364f141e6b1360ed3391b044888dcf
Johnson <oyesinajohnson@gmail.com>
2022-09-08 03:29:52 -0700

    printing using printf commands

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of C program
+ * Return:0
+ */
+
+int main(void)
+{
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
