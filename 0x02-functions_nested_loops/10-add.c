f790c8e47d91812ba4958aead56b89e5676bebd5
Johnson Oyesina <37639025+Johnnycake01@users.noreply.github.com>
2022-09-13 19:53:03 +0100

    add operation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add to integers and returns the result
+ *
+ * @num1: first number to add
+ * @num2: second number to add
+ *
+ * Return: result of num1 + num2
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
