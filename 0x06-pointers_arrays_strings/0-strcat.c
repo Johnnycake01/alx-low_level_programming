6de205955367d3cd8d7926d84da358dadc354123
Johnson Oyesina <37639025+Johnnycake01@users.noreply.github.com>
2022-09-21 21:59:58 +0100

    appennding

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenate two strings.
+ *
+ * @dest: the final string.
+ * @src: the other string.
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int i,j;
+    for(i = 0; dest[i] != '\0'; i++)
+        ;
+
+    for(j = 0; src[j] != '\0'; j++)
+    {
+        dest[i] = src[j];
+        i++;
+    }
+
+    return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointer_arrays_strings directory.
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
